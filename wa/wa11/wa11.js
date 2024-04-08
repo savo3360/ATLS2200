5a9c3fcc571a4ba20873d1c0465ac50306828794
savo3360 <savo3360@colorado.edu>
2024-04-07 22:23:35 -0600

    wow

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -8,11 +8,11 @@ const overlay = document.querySelector('.overlay');
 
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'pic1.jpg' : 'Tucker',
+  'pic2.jpg' : 'Evil Patrick',
+  'pic3.jpg' : 'Evil spongebob',
+  'pic4.jpg' : 'nice spongebob',
+  'pic5.jpg' : 'nice patrick'
 }
 
 /* Looping through images */
